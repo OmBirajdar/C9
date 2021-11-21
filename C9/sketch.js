8c3e179141b4d73fa93c2e8f00f154d9777ec78c
OmBirajdar <94775472+OmBirajdar@users.noreply.github.com>
2021-11-21 12:00:43 +0530

    Add files via upload

---
+++ C9/sketch.js
@@ -0,0 +1,34 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+box = createSprite(200,200,30,40)
+
+
+
+}
+
+function draw() 
+{
+  background(30);
+if(keyIsDown(RIGHT_ARROW)){
+  box.position.x=box.position.x+5;
+}
+
+
+
+
+
+drawSprites()
+
+
+
+
+
+
+
+
+}
+
+
+
+
